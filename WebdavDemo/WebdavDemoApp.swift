60ede962ce1d5da60b69f7dcdc6cfc5ec0d48ed7
周辉 <faichou.zh@gmail.com>
2023-12-22 14:48:58 +0800

    Initial Commit

---
+++ WebdavDemo/WebdavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebdavDemoApp.swift
+//  WebdavDemo
+//
+//  Created by FaiChou on 2023/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WebdavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
